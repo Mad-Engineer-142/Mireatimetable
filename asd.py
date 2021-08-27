df7e206d6acb8c542d8aebbd61a1bea6022ba487
Yo're-In_David <51287631+Mad-Engineer-142@users.noreply.github.com>
2021-08-27 20:21:04 +0300

    Update asd.py

--- asd.py
+++ asd.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
 import pandas as pd
 import json
 import re
+from flask import Flask, render_template
+import sys
+sys.stdout.encoding  # 'UTF-8'
 
 data = pd.read_excel('1.xlsx')
 data = data.where(pd.notnull(data), None)
 
 data_top = data.head(15) 
-data_dd = data.loc[:4] #выводит 2 строчки
-data_dd = data_dd.iloc[:,:4]#предметы data.iloc[:,3] кабинеты
+#data_dd = data.loc[:4] #выводит 2 строчки
+data_dd = data.iloc[:,:4]#предметы data.iloc[:,3] кабинеты
 
 def norm(aa):
     for i in range(len(aa)):
@@ -19,24 +21,38 @@ def norm(aa):
 
 #print(data_dd)
 
-time = norm(data_dd['Unnamed: 1'].tolist())
 
 aa = norm(data_dd['1 КУРС'].tolist())
 aud = norm(data_dd['Unnamed: 3'].tolist())
 
-print('___________')
-print(aud)
-print('___________')
-#json_dump = json.dumps(aa[0]:{[data_dd['Unnamed: 0'].tolist()[1], {'bar': ('baz', None, 1.0, 2)}]})
-json_dump = json.dumps([aa[0], {data_dd['Unnamed: 0'].tolist()[1]: [[time[1],aa[1],aud[1]], [time[2],aa[2],aud[2]], [time[3],aa[3],aud[3]], [time[4],aa[4],aud[4]]]}], ensure_ascii=False).encode('utf8')
-#'group':
 
+
+dic = {"monday":[["value", 'i','hate','niggers'],['damn','yra','is','gay']],"tuesday":["value", 'i','hate','niggers'] }
+group = aa[0]
+group = re.sub(" ", "", group)
+#print(group)
+#print('______________________')
+aa = aa[1:]
+aud = aud[1:]
+dic ={
+    "week":"0",#0 - четная, 1 - нечетная
+    group: [
+        { "monday":aa[:4],'auditory':aud[:4]},
+        { "tuesday":aa[6:10],'auditory':aud[6:10]},
+        { "wednesday":aa[12:16],'auditory':aud[12:16]},
+        { "thursday":aa[18:22],'auditory':aud[18:22]},
+        { "friday":aa[24:28],'auditory':aud[24:28]}
+     ]
+ } 
+# Вариант 1
+import json
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 @app.route('/')
 def index():
-    return render_template('a.html', variable=json_dump.decode())
+    return render_template('a.html', variable=json.dumps(dic, indent=4, sort_keys=True,ensure_ascii=False))
 
 if __name__ == '__main__':
     app.run()
